a3d7eb6b0434bb473f7a6b5bf115b2ae49924f80
Jeff Epler <jepler@gmail.com>
2024-11-04 12:05:55 -0600

    fix tests & add expected results

--- tests/circuitpython/synthio_block_biquad.py
+++ tests/circuitpython/synthio_block_biquad.py
@@ -1,5 +1,5 @@
 from synthnotehelper import *
-from synthio import BlockBiquad, FilterType
+from synthio import BlockBiquad, FilterMode
 import random
 
 random.seed(41)
@@ -10,7 +10,7 @@ white_noise = array.array('h', [random.randint(-32000, 32000) for i in range(600
 def gen(synth):
     l = LFO(sweep, offset=1440, scale=2880, rate=.025, once=True)
     yield [l]
-    b = BlockBiquad(FilterType.LOW_PASS, l)
+    b = BlockBiquad(FilterMode.LOW_PASS, l)
     n = Note(100, filter=b, waveform=white_noise)
     synth.press(n)
     yield 20
