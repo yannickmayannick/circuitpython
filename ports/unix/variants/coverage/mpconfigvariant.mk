f8afdcb72924d0d3ca7e76669d2156d18e07bec1
Jeff Epler <jepler@gmail.com>
2024-11-05 09:36:18 -0600

    Add audiodelays & audioeffects to unix coverage port
    .. and add a very basic audioeffects test, showing that it plausibly
    is working
    
    I had to address several build errors that occurred in the Unix build,
    mostly related to conversion from FP types to integral types (replaced
    by explicit casts) and by
    accidental mixing of regular & f-suffixed floating constants (replaced
    with the MICROPY_FLOAT_CONST macro)
    
    Particularly this change could use consideration:
    ```diff
    -    self->max_echo_buffer_len = self->sample_rate / 1000.0f * max_delay_ms * (self->channel_count * sizeof(uint16_t)); // bytes
    +    self->max_echo_buffer_len = (uint32_t)(self->sample_rate / 1000.0f * max_delay_ms) * (self->channel_count * sizeof(uint16_t)); // bytes
    ```
    
    The buffer length is being calculated in floating point based on the
    millisecond delay & the sample rate. The result could then be a fractional
    number such as 529.2 for a 12ms delay at 44.1kHz. Multiplying a floating
    number by the size required for each echo buffer item
    (`(self->channel_count * sizeof(uint16_t))`) could yield a number of bytes
    that doesn't correspond to an integral number of buffer items. I grouped
    the float->int conversion so that it converts the number of echo buffer
    items to an integer and then multiplies by the size of the item.

--- ports/unix/variants/coverage/mpconfigvariant.mk
+++ ports/unix/variants/coverage/mpconfigvariant.mk
@@ -33,6 +33,10 @@ SRC_BITMAP := \
 	shared-bindings/audiocore/__init__.c \
 	shared-bindings/audiocore/RawSample.c \
 	shared-bindings/audiocore/WaveFile.c \
+	shared-bindings/audiodelays/Echo.c \
+	shared-bindings/audiodelays/__init__.c \
+	shared-bindings/audiofilters/Filter.c \
+	shared-bindings/audiofilters/__init__.c \
 	shared-bindings/audiomixer/__init__.c \
 	shared-bindings/audiomixer/Mixer.c \
 	shared-bindings/audiomixer/MixerVoice.c \
@@ -70,6 +74,10 @@ SRC_BITMAP := \
 	shared-module/audiocore/__init__.c \
 	shared-module/audiocore/RawSample.c \
 	shared-module/audiocore/WaveFile.c \
+	shared-module/audiodelays/Echo.c \
+	shared-module/audiodelays/__init__.c \
+	shared-module/audiofilters/Filter.c \
+	shared-module/audiofilters/__init__.c \
 	shared-module/audiomixer/__init__.c \
 	shared-module/audiomp3/MP3Decoder.c \
 	shared-module/audiomixer/Mixer.c \
@@ -127,6 +135,9 @@ $(BUILD)/lib/mp3/src/buffers.o: CFLAGS += -include "shared-module/audiomp3/__ini
 CFLAGS += \
 	-DCIRCUITPY_AESIO=1 \
 	-DCIRCUITPY_AUDIOCORE=1 \
+	-DCIRCUITPY_AUDIOEFFECTS=1 \
+	-DCIRCUITPY_AUDIODELAYS=1 \
+	-DCIRCUITPY_AUDIOFILTERS=1 \
 	-DCIRCUITPY_AUDIOMIXER=1 \
 	-DCIRCUITPY_AUDIOMP3=1 \
 	-DCIRCUITPY_AUDIOMP3_USE_PORT_ALLOCATOR=0 \
