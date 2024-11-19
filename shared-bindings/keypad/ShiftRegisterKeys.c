d08cecfeb82b7d13027d0275f0c2dc52886614e3
Dan Halbert <halbert@halwitz.org>
2024-11-19 17:45:25 -0500

    keypad: add doc that reset() can be used to determine keys held down at startup

--- shared-bindings/keypad/ShiftRegisterKeys.c
+++ shared-bindings/keypad/ShiftRegisterKeys.c
@@ -201,6 +201,9 @@ static MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(keypad_shiftregisterkeys___exit___obj
 //|         """Reset the internal state of the scanner to assume that all keys are now released.
 //|         Any key that is already pressed at the time of this call will therefore immediately cause
 //|         a new key-pressed event to occur.
+//|         For instance, if you call `reset()` immediately after creating a `ShiftRegisterKeys` object
+//|         at the beginning of your program, the events it generates will let you determine which keys
+//|         were being held down at program start.
 //|         """
 //|         ...
 
