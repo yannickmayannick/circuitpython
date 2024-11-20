f14b4c6636e769fecdc8c24b51e7bd6f7dbebd5a
Tim <timchinowsky@gmail.com>
2024-11-19 17:36:54 -0800

    add background read files

--- ports/raspberrypi/audio_dma.c
+++ ports/raspberrypi/audio_dma.c
@@ -480,7 +480,25 @@ void __not_in_flash_func(isr_dma_0)(void) {
         }
         if (MP_STATE_PORT(background_pio)[i] != NULL) {
             rp2pio_statemachine_obj_t *pio = MP_STATE_PORT(background_pio)[i];
-            rp2pio_statemachine_dma_complete(pio, i);
+            rp2pio_statemachine_dma_complete_write(pio, i);
+        }
+    }
+}
+
+void __not_in_flash_func(isr_dma_1)(void) {
+    for (size_t i = 0; i < NUM_DMA_CHANNELS; i++) {
+        uint32_t mask = 1 << i;
+        if ((dma_hw->intr & mask) == 0) {
+            continue;
+        }
+        // acknowledge interrupt early. Doing so late means that you could lose an
+        // interrupt if the buffer is very small and the DMA operation
+        // completed by the time callback_add() / dma_complete() returned. This
+        // affected PIO continuous write more than audio.
+        dma_hw->ints1 = mask;
+        if (MP_STATE_PORT(background_pio)[i] != NULL) {
+            rp2pio_statemachine_obj_t *pio = MP_STATE_PORT(background_pio)[i];
+            rp2pio_statemachine_dma_complete_read(pio, i);
         }
     }
 }
