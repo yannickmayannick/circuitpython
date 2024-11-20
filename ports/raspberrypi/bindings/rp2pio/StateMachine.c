8d561a0b0e69741cca7733d1a64e52d6ebd2b825
Tim <timchinowsky@gmail.com>
2024-11-20 11:21:21 -0800

    add pre-commit check

--- ports/raspberrypi/bindings/rp2pio/StateMachine.c
+++ ports/raspberrypi/bindings/rp2pio/StateMachine.c
@@ -506,7 +506,7 @@ MP_DEFINE_CONST_FUN_OBJ_KW(rp2pio_statemachine_write_obj, 2, rp2pio_statemachine
 //|         Then the ``once`` and/or ``loop`` buffers are queued. and the function returns.
 //|         The ``once`` buffer (if specified) will be written just once.
 //|         Finally, the ``loop`` and/or ``loop2`` buffer (if specified) will continue being looped indefinitely.  If both ``loop`` and ``loop2`` are specified, they will alternate.
-//|         
+//|
 //|         Writes to the FIFO will match the input buffer's element size. For example, bytearray elements
 //|         will perform 8 bit writes to the PIO FIFO. The RP2040's memory bus will duplicate the value into
 //|         the other byte positions. So, pulling more data in the PIO assembly will read the duplicated values.
