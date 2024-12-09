2340317b25069ec03556be9cc5889bf456a3c258
Timeline8 <ptimlin67@gmail.com>
2024-12-09 00:01:45 -0500

    Update ports/espressif/boards/waveshare_esp32_s3_zero/pins.c
    Co-authored-by: Scott Shawcroft <scott@tannewt.org>

--- ports/espressif/boards/waveshare_esp32_s3_zero/pins.c
+++ ports/espressif/boards/waveshare_esp32_s3_zero/pins.c
@@ -9,7 +9,7 @@
 static const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
 
-    // BOOT button labeled simply as "B" om silkscreen
+    // BOOT button labeled simply as "B" on silkscreen
     { MP_ROM_QSTR(MP_QSTR_BUTTON), MP_ROM_PTR(&pin_GPIO0) },
     { MP_ROM_QSTR(MP_QSTR_D0), MP_ROM_PTR(&pin_GPIO0) },
 
