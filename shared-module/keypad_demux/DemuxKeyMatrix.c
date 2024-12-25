fdb38ccc1103ab7e9217754b78749ae4a7a2bdf2
Patrick Surry <patrick.surry@gmail.com>
2024-12-25 13:29:47 -0500

    wip: include transpose and columns_to_anodes options

--- shared-module/keypad_demux/DemuxKeyMatrix.c
+++ shared-module/keypad_demux/DemuxKeyMatrix.c
@@ -27,11 +27,15 @@ static keypad_scanner_funcs_t keymatrix_funcs = {
 };
 
 static mp_uint_t row_column_to_key_number(keypad_demux_demuxkeymatrix_obj_t *self, mp_uint_t row, mp_uint_t column) {
-    return row * self->column_digitalinouts->len + column;
+    // return the key index in the user's coordinate system
+    return row * common_hal_keypad_demux_demuxkeymatrix_get_column_count(self) + column;
 }
 
-void common_hal_keypad_demux_demuxkeymatrix_construct(keypad_demux_demuxkeymatrix_obj_t *self, mp_uint_t num_row_addr_pins, const mcu_pin_obj_t *row_addr_pins[], mp_uint_t num_column_pins, const mcu_pin_obj_t *column_pins[], mp_float_t interval, size_t max_events, uint8_t debounce_threshold) {
+void common_hal_keypad_demux_demuxkeymatrix_construct(keypad_demux_demuxkeymatrix_obj_t *self, mp_uint_t num_row_addr_pins, const mcu_pin_obj_t *row_addr_pins[], mp_uint_t num_column_pins, const mcu_pin_obj_t *column_pins[], bool columns_to_anodes, bool transpose, mp_float_t interval, size_t max_events, uint8_t debounce_threshold) {
 
+    // the multiplexed pins are outputs so we can set the address for the target row
+    // the sense of the address pins themselves doesn't change with columns_to_anodes
+    // but the value output on the selected row line will be !columns_to_anodes
     mp_obj_t row_addr_dios[num_row_addr_pins];
     for (size_t row = 0; row < num_row_addr_pins; row++) {
         digitalio_digitalinout_obj_t *dio =
@@ -42,17 +46,20 @@ void common_hal_keypad_demux_demuxkeymatrix_construct(keypad_demux_demuxkeymatri
     }
     self->row_addr_digitalinouts = mp_obj_new_tuple(num_row_addr_pins, row_addr_dios);
 
+    // the column pins are always inputs, with default state based on columns_to_anodes
     mp_obj_t column_dios[num_column_pins];
     for (size_t column = 0; column < num_column_pins; column++) {
         digitalio_digitalinout_obj_t *dio =
             mp_obj_malloc(digitalio_digitalinout_obj_t, &digitalio_digitalinout_type);
         dio->base.type = &digitalio_digitalinout_type;
         common_hal_digitalio_digitalinout_construct(dio, column_pins[column]);
-        common_hal_digitalio_digitalinout_switch_to_input(dio, PULL_UP);
+        common_hal_digitalio_digitalinout_switch_to_input(dio, columns_to_anodes ? PULL_UP : PULL_DOWN);
         column_dios[column] = dio;
     }
     self->column_digitalinouts = mp_obj_new_tuple(num_column_pins, column_dios);
 
+    self->columns_to_anodes = columns_to_anodes;
+    self->transpose = transpose;
     self->funcs = &keymatrix_funcs;
 
     keypad_construct_common((keypad_scanner_obj_t *)self, interval, max_events, debounce_threshold);
@@ -79,11 +86,15 @@ void common_hal_keypad_demux_demuxkeymatrix_deinit(keypad_demux_demuxkeymatrix_o
 }
 
 size_t common_hal_keypad_demux_demuxkeymatrix_get_row_count(keypad_demux_demuxkeymatrix_obj_t *self) {
-    return 1 << self->row_addr_digitalinouts->len;
+    return self->transpose
+        ? self->column_digitalinouts->len
+        : (size_t)(1 << self->row_addr_digitalinouts->len);
 }
 
 size_t common_hal_keypad_demux_demuxkeymatrix_get_column_count(keypad_demux_demuxkeymatrix_obj_t *self) {
-    return self->column_digitalinouts->len;
+    return self->transpose
+        ? (size_t)(1 << self->row_addr_digitalinouts->len)
+        : self->column_digitalinouts->len;
 }
 
 mp_uint_t common_hal_keypad_demux_demuxkeymatrix_row_column_to_key_number(keypad_demux_demuxkeymatrix_obj_t *self, mp_uint_t row, mp_uint_t column) {
@@ -105,11 +116,11 @@ static void demuxkeymatrix_scan_now(void *self_in, mp_obj_t timestamp) {
     keypad_demux_demuxkeymatrix_obj_t *self = self_in;
 
     for (size_t row = 0; row < common_hal_keypad_demux_demuxkeymatrix_get_row_count(self); row++) {
-        // Set the row address on the demultiplexer.  This currently assumes an inverting
-        // demux like the 74hc138 which outputs low on the selected line and high on non-selected ones.
-        // This class should probably support a columns_to_anodes parameter where this case would
-        // be True (the row gets pulled low), and a non-inverting demux like 74hc238 would
-        // set columns_to_anodes=False.
+        // Set the row address on the demultiplexer.
+        // When columns_to_anodes is True (default) we've got an inverting demux
+        // so the selected line is pulled low, and we look for rows that get pulled low.
+        // When columns_to_anodes is False we do the reverse.
+        // We can safely ignore transposition since it's handled by row_column_to_key_number
         size_t mask = 0b00000001;
         for (size_t row_addr_pin = 0; row_addr_pin < self->row_addr_digitalinouts->len; row_addr_pin++) {
             digitalio_digitalinout_obj_t *dio = self->row_addr_digitalinouts->items[row_addr_pin];
@@ -128,9 +139,11 @@ static void demuxkeymatrix_scan_now(void *self_in, mp_obj_t timestamp) {
         for (size_t column = 0; column < common_hal_keypad_demux_demuxkeymatrix_get_column_count(self); column++) {
             mp_uint_t key_number = row_column_to_key_number(self, row, column);
 
-            // Get the current state, by reading whether the column got pulled to the row value or not.
-            // (with a columns_to_anodes parameter this could mimic the KeyMatrix code)
-            const bool current = !common_hal_digitalio_digitalinout_get_value(self->column_digitalinouts->items[column]);
+            // Get the current state, by reading whether the column got pulled to the row value or not,
+            // which is the opposite of columns_to_anodes.
+            const bool current =
+                common_hal_digitalio_digitalinout_get_value(self->column_digitalinouts->items[column]) !=
+                self->columns_to_anodes;
 
             // Record any transitions.
             if (keypad_debounce((keypad_scanner_obj_t *)self, key_number, current)) {
