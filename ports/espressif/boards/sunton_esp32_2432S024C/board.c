cd2c6c4de968f4f8deccdb22629f63eaa8c3b12e
Olav Schettler <olav@schettler.net>
2024-12-27 23:01:33 +0100

    sunton_esp32_2432S024C: Fixed pins, tested screen, touch, led, sdcard

--- ports/espressif/boards/sunton_esp32_2432S024C/board.c
+++ ports/espressif/boards/sunton_esp32_2432S024C/board.c
@@ -1,6 +1,6 @@
 // This file is part of the CircuitPython project: https://circuitpython.org
 //
-// SPDX-FileCopyrightText: Copyright (c) 2020 Scott Shawcroft for Adafruit Industries
+// SPDX-FileCopyrightText: Copyright (c) 2024 Olav Schettler
 //
 // SPDX-License-Identifier: MIT
 
@@ -82,7 +82,7 @@ static void display_init(void) {
         MIPI_COMMAND_WRITE_MEMORY_START, // Write memory command
         display_init_sequence,
         sizeof(display_init_sequence),
-        &pin_GPIO21,  // backlight pin
+        &pin_GPIO27,  // backlight pin
         NO_BRIGHTNESS_COMMAND,
         1.0f, // brightness
         false, // single_byte_bounds
