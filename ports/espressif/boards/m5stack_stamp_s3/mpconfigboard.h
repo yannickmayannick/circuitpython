1d138452ecf6e500e42c9a390595a0dae53ca0ae
Stella <contact@stellas.fun>
2025-01-13 13:36:44 -0500

    Update ports/espressif/boards/m5stack_stamp_s3/mpconfigboard.h
    Co-authored-by: Scott Shawcroft <scott@tannewt.org>

--- ports/espressif/boards/m5stack_stamp_s3/mpconfigboard.h
+++ ports/espressif/boards/m5stack_stamp_s3/mpconfigboard.h
@@ -1,6 +1,6 @@
 // This file is part of the CircuitPython project: https://circuitpython.org
 //
-// SPDX-FileCopyrightText: 2025 Stella Schwankl for Adafruit Industries
+// SPDX-FileCopyrightText: 2025 Stella Schwankl
 //
 // SPDX-License-Identifier: MIT
 
