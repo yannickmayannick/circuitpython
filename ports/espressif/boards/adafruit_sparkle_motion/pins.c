31701e3e9da31bc4d6ac515041722cde729b11db
BlitzCityDIY <blitzcitydiy@gmail.com>
2025-01-15 13:46:51 -0500

    add comma

--- ports/espressif/boards/adafruit_sparkle_motion/pins.c
+++ ports/espressif/boards/adafruit_sparkle_motion/pins.c
@@ -53,7 +53,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_D18), MP_ROM_PTR(&pin_GPIO18) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
-    { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) }
+    { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
