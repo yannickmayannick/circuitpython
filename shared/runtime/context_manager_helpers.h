8da339e2f8532926331a8a3840d7940490611ea6
Jeff Epler <jepler@gmail.com>
2025-02-07 20:49:29 -0600

    context_manager_helpers: Comment on what default enter & exit obj are

--- shared/runtime/context_manager_helpers.h
+++ shared/runtime/context_manager_helpers.h
@@ -8,5 +8,13 @@
 
 #include "py/obj.h"
 
+// An object that has a `deinit` method can use `default___enter___obj` and
+// `default___exit___obj` to define the `__enter__` and `__exit__` members in
+// its object table.
+//
+// `__enter__` returns the object itself, and `__exit__` calls its `deinit`.
+//
+// If enter/exit do anything else, such as taking & releasing a lock, these are
+// not suitable.
 #define default___enter___obj (mp_identity_obj)
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(default___exit___obj);
