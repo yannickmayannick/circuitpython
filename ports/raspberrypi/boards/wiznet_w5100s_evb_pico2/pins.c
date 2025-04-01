5e28e2d56a0cb02fff36840bb6a8a3d57ad8fd1e
Edward <fasteddy@thewrightspace.net>
2025-04-01 14:26:08 -0400

    Rename .SPI() to .W5K_SPI()

--- ports/raspberrypi/boards/wiznet_w5100s_evb_pico2/pins.c
+++ ports/raspberrypi/boards/wiznet_w5100s_evb_pico2/pins.c
@@ -70,7 +70,7 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_A3), MP_ROM_PTR(&pin_GPIO29) },
     { MP_ROM_QSTR(MP_QSTR_VOLTAGE_MONITOR), MP_ROM_PTR(&pin_GPIO29) },
 
-    { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
+    { MP_ROM_QSTR(MP_QSTR_W5K_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
 };
