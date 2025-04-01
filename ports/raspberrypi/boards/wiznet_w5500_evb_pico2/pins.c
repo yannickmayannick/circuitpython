b92bf198b98c7d0f903649190eda41995e19002b
Edward <fasteddy@thewrightspace.net>
2025-04-01 14:48:07 -0400

    Match defaults across all WIZnet boards.

--- ports/raspberrypi/boards/wiznet_w5500_evb_pico2/pins.c
+++ ports/raspberrypi/boards/wiznet_w5500_evb_pico2/pins.c
@@ -70,6 +70,8 @@ static const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_A3), MP_ROM_PTR(&pin_GPIO29) },
     { MP_ROM_QSTR(MP_QSTR_VOLTAGE_MONITOR), MP_ROM_PTR(&pin_GPIO29) },
 
+    { MP_ROM_QSTR(MP_QSTR_W5K_SPI), MP_ROM_PTR(&board_spi_obj) },
+    { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
